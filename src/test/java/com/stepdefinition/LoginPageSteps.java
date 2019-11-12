a3d6d06fdaf4a300395dd6dc7df08d9b293bbbc6
Swetha751 <swetharavi751@gmail.com>
2019-11-12 09:47:01 +0530

    new code uploaded

---
+++ src/test/java/com/stepdefinition/LoginPageSteps.java
@@ -0,0 +1,39 @@
+package com.stepdefinition;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class LoginPageSteps {
+	WebDriver driver;
+	@Given("User is on AdactIn page")
+	public void user_is_on_AdactIn_page() {
+	   System.setProperty("webdriver.chrome.driver", "C:\\Users\\Swetha\\eclipse-workspace-Swetha\\Cucumber\\Driver\\chromedriver.exe");
+	    driver = new ChromeDriver();
+	    driver.get("http://adactin.com/HotelApp/");
+	    driver.manage().window().maximize();
+	    }
+
+	@When("User enters username and password")
+	public void user_enters_username_and_password() {
+		WebElement user = driver.findElement(By.id("username"));
+		user.sendKeys("Swetha");
+		WebElement pass = driver.findElement(By.id("password"));
+	   pass.sendKeys("Hello");
+	    }
+
+	@Then("User click the login button")
+	public void user_click_the_login_button() {
+		WebElement btnlogin = driver.findElement(By.id("login"));
+		btnlogin.click();
+	    }
+
+
+	
+
+}
---
+++ src/test/resources/Features/Login Page.feature
@@ -0,0 +1,5 @@
+Feature: Validation of adactIn page
+Scenario: Login validation
+Given User is on AdactIn page
+When User enters username and password
+Then User click the login button
